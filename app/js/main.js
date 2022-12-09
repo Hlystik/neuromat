6f4a15d220e0eabc188132c42878af5cf8fd235d
Anatoly <hlystike@rambler.ru>
2022-12-10 01:48:01 +0700

    desctop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function () {
+   /*  $('.acc__head').on('click', function () {
+        console.log = (this);
+        $(this).toggleClass('acc__head_active').next().slideToggle();
+        $('.acc__head').not(this).removeClass('acc__head_active').next().slideUp();
+      });
+      $('#acc .acc__head:first').click(); */
+      $('.accordion-list > li > .answer').hide();
+    
+  $('.accordion-list > li').click(function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active").find(".answer").slideUp();
+    } else {
+      $(".accordion-list > li.active .answer").slideUp();
+      $(".accordion-list > li.active").removeClass("active");
+      $(this).addClass("active").find(".answer").slideDown();
+    }
+    return false;
+  });
+  
+
 
 })
